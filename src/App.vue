8114e1dc0d156c81381601bf99d62aa41d8285d1
Justin Urbanek <103039882+Egomann88@users.noreply.github.com>
2022-08-19 16:15:55 +0200

    Initialize prop

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div style="min-height: 100vh;">
+    <Main />
+  </div>
+  <Footer />
+</template>
+
+<script setup lang="ts">
+import Main from './components/Main.vue'
+import Footer from './components/Footer.vue'
+</script>
+
+<style scoped>
+</style>
