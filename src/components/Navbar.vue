7e268b67199805a7bccfea49dd169c0575b01b0a
Justin <justin@kauz.ch>
2022-08-23 10:29:16 +0200

    Navbar created

---
+++ src/components/Navbar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <section class="w-full mx-auto">
+      <div class="relative bg-gray-200 p-4">
+        <button class="btn-Header">Blackjack</button>
+        <button class="btn-Header">Shop</button>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  components: {},
+  data() {
+    let i = 0;
+
+    return { i };
+  },
+  setup() {},
+  mounted() {},
+  methods: {},
+});
+</script>
+
+<style lang="css" scoped></style>
