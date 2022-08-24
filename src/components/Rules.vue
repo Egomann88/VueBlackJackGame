ddf1947cc2eaaebd292fed7d3214932eb25804da
Justin <justin@kauz.ch>
2022-08-24 09:47:49 +0200

    misspelling in rules fixed

--- src/components/Rules.vue
+++ src/components/Rules.vue
@@ -2,7 +2,8 @@
   <div>
     <section v-if="showRules" class="fixed bg-gray-200 rounded-r-xl top-1/2 -translate-y-1/2 max-w-lg z-40 text-left">
       <div class="max-h-[65vh] overflow-auto py-4 px-6 relative">
-        <button @click="toggleRules()" class="btn hover:scale-110 absolute right-10 top-10 border-2 border-black">X</button>
+        <button @click="toggleRules()"
+          class="btn hover:scale-110 absolute right-10 top-10 border-2 border-black">X</button>
         <div class="sm:px-2">
           <h1>Rules</h1>
           <h2>Game Rules</h2>
@@ -10,14 +11,14 @@
             <li>The Dealer always has an hidden Card, which will be revealed, when the round is over.</li>
             <li>The Dealer draws before the player.</li>
             <li>The Dealer has higher Priority, means when both player and dealer's card values are over 21. The Dealer
-              still wins</li>
+              still wins.</li>
           </ul>
           <h2>Card Values</h2>
           <ul>
-            <li>Cards 2-10 are worth the value of the number on the face of the card.
+            <li>Cards 2-10 are worth the value of the number on the face of the Card.
             </li>
             <li>
-              Face cards are worth 10, except for the Ace, which is worth 1 or 11.
+              Face Cards are worth 10, except for the Ace, which is worth 1 or 11.
             </li>
           </ul>
           <h2>21 Card Game</h2>
