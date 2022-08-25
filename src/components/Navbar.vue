b88eb72e5861baee4678678651743f5d47506af9
Justin <justin@kauz.ch>
2022-08-25 09:58:33 +0200

    Navbar Upgrades added & margin for mobile

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,9 +2,10 @@
   <div>
     <section class="w-full mx-auto">
       <div class="relative bg-gray-200 p-4">
-        <router-link to="/" class="btn-Header inline-block text-lg">Blackjack
+        <router-link to="/" class="btn-Header m-2 inline-block text-lg">Blackjack
         </router-link>
-        <router-link to="/shop" class="btn-Header inline-block text-lg">Shop</router-link>
+        <router-link to="/shop" class="btn-Header m-2 inline-block text-lg">Shop</router-link>
+        <router-link to="/upgrades" class="btn-Header m-2 inline-block text-lg">Upgrades</router-link>
       </div>
     </section>
   </div>
